5de7f2d02b9ef049f302c96dd61aaa9be18dd59a
JoaoNevesSoares <jemplay00@gmail.com>
2020-10-17 19:22:16 -0300

    alteração exer_29 e printscreen de informações memory Valgrind

--- exer_slide29.c
+++ exer_slide29.c
@@ -5,22 +5,35 @@ TODOS OS NOMES ARMAZENADOS EM UM UNICO VETOR*/
 #include <string.h>
 #define LOOP 1
 
-void remover(char **string,char *word){
+char *string_hold=NULL;
+void remover(char *word){
 
-   char *temp = *string;
-    char *subs = malloc(strlen(temp)-strlen(word));
-    char *x = malloc(strlen(word)+2);
+   char *temp = string_hold;
+    char *subs = (char *)calloc(strlen(temp)-strlen(word),sizeof(char));
+    if(subs == NULL){
+        printf("Problema de alocação de memoria, tente novamente\n");
+        return;
+    }
+    char *x = (char *)calloc(strlen(word)+2,sizeof(char));
+    if(x == NULL){
+        printf("problema de alocação de memoria, tente novamente");
+        return;
+    }
     //x[0]='/';
     strcat(x,word);
     strcat(x,"/");
-    if(strstr(*string,word)==NULL){
+    if(strstr(string_hold,word)==NULL){
     printf("Não existe esse nome na lista, tente novamente ou saia do programa\n");
     return;
     }
     while(*temp){
         
         
-        char *token = (char *)malloc(20);
+        char *token = (char *)calloc(100,sizeof(char));
+        if(token == NULL){
+            printf("Problema de alocação de memoria tente novamente \n");
+            return;
+        }
         char *head=token;
         for(;*temp!='/';temp++){
             *token = *temp;
@@ -34,34 +47,49 @@ void remover(char **string,char *word){
         token = head;
         free(token);
     }
-    *string = subs;
+    free(string_hold);
+    string_hold = subs;
     free(x);
 }
-void adicionar(char **string){
+void adicionar(){
 
     char *word = (char *)malloc(50*sizeof(char));
-    char *temp=NULL;
+    if(word == NULL){
+        printf("Problema de alocação de memoria tente novamente\n");
+        return;
+    }
     scanf("%s",word);
     int word_len = strlen(word);
     strcat(word,"/");
     
-    if(*string==NULL){
-        *string=(char *)malloc(word_len*sizeof(char));
-        strcat(*string,word);
+    if(string_hold==NULL){
+        string_hold=(char *)malloc(word_len*sizeof(char));
+        if(string_hold==NULL){
+            printf("Problema de alocação de memoria, tente novamente\n");
+            return;
+        }
+        strcat(string_hold,word);
     }
     else{
-        int str_len = strlen(*string);
+        int str_len = strlen(string_hold);
         //temp = (char*)realloc(*string,str_len+word_len);
-        *string = (char*)realloc(*string,str_len+word_len);
-        strcat(*string,word);
+        string_hold = (char*)realloc(string_hold,str_len+word_len);
+        if(string_hold ==NULL){
+            printf("Problema de alocação de memoria, tente novamente\n");
+        }
+        strcat(string_hold,word);
     }
     free(word);
 }
-void listar(char *string){
-
-    for(;*string!='\0';string++){
-        if(*string !='/'){
-        printf("%c",*string);
+void listar(){
+    char *temp = string_hold;
+    if(temp == NULL){
+        printf("A lista está vazia\n");
+        return;
+    }
+    for(;*temp!='\0';temp++){
+        if(*temp !='/'){
+        printf("%c",*temp);
         }
         else{
             printf("\n");
@@ -71,9 +99,8 @@ void listar(char *string){
 int main(){
 
 
-    char *string_hold=NULL;
     int s;
-    printf("--->MENU DE OPÇÕES<---\nL ---> listar\na ---> Adicionar nome\nr ---> Remover nome (digitar o nome)\ns ---> Sair do programa\n");
+    printf("--->MENU DE OPÇÕES<---\nz ---> listar\na ---> Adicionar nome\nr ---> Remover nome (digitar o nome)\ns ---> Sair do programa\n");
     while(LOOP){
         
         
@@ -83,13 +110,12 @@ int main(){
         {
         case 'z':
             
-            listar(string_hold);
+            listar();
             printf("aperte qualquer coisa para sair\n");
             int g = getchar();
             break;
         case 'a':
-            adicionar(&string_hold);
-            free(string_hold);
+            adicionar();
 
             break;
 
@@ -97,9 +123,10 @@ int main(){
             printf("digite o nome para remover\n");
             char nome[100];
             scanf("%s",nome);
-            remover(&string_hold,nome);
+            remover(nome);
             break;
         case 's':
+            free(string_hold);
             return 0;
             break;
         default:
@@ -109,11 +136,8 @@ int main(){
          printf("--->MENU DE OPÇÕES<---\nL ---> listar\na ---> Adicionar nome\nr ---> Remover nome (digitar o nome)\ns ---> Sair do programa\n");
 
     }
+    return 0;
     
-
-
-
-
 
 
 }
